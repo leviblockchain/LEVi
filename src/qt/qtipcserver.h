896a6eea90ffaac656b2ab46a8f85a63071b6852
leviblockchain <admin@levichain.net>
2017-10-07 04:47:45 +0100

    LEVi source

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define LEVi-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "LEViURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
